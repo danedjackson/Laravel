dd27a67afa7c93b8d059384f67cba8b80707f0e4
danedjackson@yahoo.com <Dane_Jackson@sagicor.com>
2024-05-30 07:27:47 -0500

    Adding JobLocation table. Issue with artisan tinker create: job_id Mass Assign issue. Fix: artisan config:cache -> artisan cache:clear

---
+++ app/Models/JobLocation.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JobLocation extends Model
+{
+    use HasFactory;
+    protected $fillable = ['job_id', 'address_line_1', 'city', 'isRemote'];
+}
+
+?>
---
+++ database/migrations/2024_05_29_202843_create_job_locations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('job_locations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('job_id');
+            $table->foreign('job_id')->references('id')->on('job_listings')->onDelete('cascade');
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('city');
+            $table->boolean('isRemote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('job_locations');
+    }
+};
